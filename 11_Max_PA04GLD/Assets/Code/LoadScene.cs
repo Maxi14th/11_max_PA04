34388627821befe92e5c3ae1caac588a0ee5b1e9
Gan Wen Hao Isaac <isaacgan64@gmai.com>
2020-05-04 10:52:36 +0800

    Updated Unity Project

---
+++ 11_Max_PA04GLD/Assets/Code/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{ 
+    public float delay = 3;
+    public string NewLevel = "SampleScene";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay)); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
